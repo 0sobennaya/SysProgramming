e065a02e823a885a26121aa3b1bc786de9e83347
0sobennaya <143941641+0sobennaya@users.noreply.github.com>
2025-03-24 21:29:48 +0300

    lab1 works

--- SysProgDmitrieva/SysProgDmitrieva/SysProgDmitrieva.cpp
+++ SysProgDmitrieva/SysProgDmitrieva/SysProgDmitrieva.cpp
@@ -18,12 +18,6 @@ DWORD WINAPI MyThread(LPVOID lpParameter)
 				delete session;
 				return 0;
 			}
-			/*case MT_DATA:
-			{
-				SafeWrite("session", session->sessionID, "data", m.data);
-				Sleep(500 * session->sessionID);
-				break;
-			}*/
 			}
 		}
 	}
@@ -41,7 +35,7 @@ void start()
 	HANDLE hCloseEvent = CreateEvent(NULL, FALSE, FALSE, L"CloseEvent");
 	HANDLE hControlEvents[3] = { hStartEvent, hStopEvent , hCloseEvent};
 	int i = 0;
-	SetEvent(hConfirmEvent);
+	
 
 	while (i >=0)
 	{
@@ -54,19 +48,16 @@ void start()
 			SetEvent(hConfirmEvent);
 			break;
 		case 1:
-			if (!sessions.empty())
-			{
-				sessions.back()->addMessage(MT_CLOSE);
-				sessions.pop_back();
-				--i;
+			if (i == 0) {
+				SetEvent(hCloseEvent);
 				SetEvent(hConfirmEvent);
 				break;
 			}
-			else
-			{
-				SetEvent(hConfirmEvent);
-				return;
-			}
+			sessions.back()->addMessage(MT_CLOSE);
+			sessions.pop_back();
+			SetEvent(hConfirmEvent);
+			i--;
+			break;
 		case 2:
 			SetEvent(hConfirmEvent);
 			return;
@@ -81,7 +72,52 @@ void start()
 
 int main()
 {
-    start();
+	std::vector<Session*> sessions;
+	int i = 0;
+
+	HANDLE hStartEvent = CreateEvent(NULL, FALSE, FALSE, L"StartEvent");
+	HANDLE hStopEvent = CreateEvent(NULL, FALSE, FALSE, L"StopEvent");
+	HANDLE hConfirmEvent = CreateEvent(NULL, FALSE, FALSE, L"ConfirmEvent");
+	HANDLE hCloseEvent = CreateEvent(NULL, FALSE, FALSE, L"CloseEvent");
+	HANDLE hControlEvents[3] = { hStartEvent, hStopEvent, hCloseEvent };
+
+	while (i >= 0) {
+		int n = WaitForMultipleObjects(3, hControlEvents, FALSE, INFINITE) - WAIT_OBJECT_0;
+		switch (n)
+		{
+		case 0:
+		{
+			sessions.push_back(new Session(i++));
+			CloseHandle(CreateThread(NULL, 0, MyThread, (LPVOID)sessions.back(), 0, NULL));
+			SetEvent(hConfirmEvent);
+			break;
+		}
+		case 1:
+		{
+			if (i == 0) {
+				SetEvent(hCloseEvent);
+				break;
+			}
+			sessions.back()->addMessage(MT_CLOSE);
+			sessions.pop_back();
+			SetEvent(hConfirmEvent);
+			i--;
+			break;
+		}
+		case 2:
+		{
+			sessions.clear();
+			SetEvent(hConfirmEvent);
+			break;
+		}
+
+		}
+	}
+	SetEvent(hConfirmEvent);
+
+
+	/*return 0;
+    start();*/
 	return 0;
 }
 
